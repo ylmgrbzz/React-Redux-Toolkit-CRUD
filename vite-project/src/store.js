454d897afae534f07aa354162c0bf48fedf869fb
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-12-20 20:39:27 +0300

    add Redux Provider and configure movie reducer in store

--- vite-project/src/store.js
+++ vite-project/src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import movieReducer from "./movieSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    movies: movieReducer,
+  },
 });
