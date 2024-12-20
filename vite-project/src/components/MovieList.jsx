f5bd2442366074960440e1ba897ba6e53ce83b32
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-12-20 20:54:44 +0300

    update MovieList component to display movie details and initialize movie state with sample data

--- vite-project/src/components/MovieList.jsx
+++ vite-project/src/components/MovieList.jsx
@@ -1,5 +1,19 @@
+import { useSelector } from "react-redux";
+
 const MovieList = () => {
-  return <div>MovieList</div>;
+  const movies = useSelector((state) => state.movies.movies);
+
+  return (
+    <div>
+      <h1>movie list</h1>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+          <p>{movie.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MovieList;
--- vite-project/src/movieSlice.js
+++ vite-project/src/movieSlice.js
@@ -1,7 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  movies: [],
+  movies: [
+    {
+      id: 1,
+      title: "The Shawshank Redemption",
+      director: "Frank Darabont",
+      rating: 9.3,
+      releaseDate: "1994-10-14",
+      runtime: 142,
+      genre: ["Drama"],
+      cast: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
+      plot: "Two imprisoned",
+    },
+    {
+      id: 2,
+      title: "The Godfather",
+      director: "Francis Ford Coppola",
+      rating: 9.2,
+      releaseDate: "1972-03-24",
+      runtime: 175,
+      genre: ["Crime", "Drama"],
+      cast: ["Marlon Brando", "Al Pacino", "James Caan"],
+      plot: "The aging patriarch",
+    },
+    {
+      id: 3,
+      title: "The Dark Knight",
+      director: "Christopher Nolan",
+      rating: 9.0,
+      releaseDate: "2008-07-18",
+      runtime: 152,
+      genre: ["Action", "Crime", "Drama"],
+      cast: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
+      plot: "When the menace",
+    },
+    {
+      id: 4,
+      title: "12 Angry",
+      director: "Sidney Lumet",
+      rating: 8.9,
+      releaseDate: "1957-04-10",
+      runtime: 96,
+      genre: ["Drama"],
+      cast: ["Henry Fonda", "Lee J. Cobb", "Martin Balsam"],
+      plot: "The defense and the prosecution",
+    },
+  ],
 };
 
 const movieSlice = createSlice({
